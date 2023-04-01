add8e321a05b9316f5dd2ee6a5d702a73c1d2540
Paul Figueroa <pjsfigueroa@gmail.com>
2023-04-01 16:05:43 +0800

    Add the hero gradient composite.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,8 @@ import { robot, discount } from '../assets'
 import GetStarted from './GetStarted'
 
 const Hero = () => (
-  <section id="home" className={`${styles.paddingY} flex-container md:flex-row flex-col`}>
+  <section id="home" className={`${styles.paddingY} flex flex-container md:flex-row flex-col`}>
+    {/* Left side */}
     <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
       <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
         <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
@@ -18,11 +19,22 @@ const Hero = () => (
           <span className="text-gradient">Generation </span>
           <br className="sm:block hidden" /> Payment Method
         </h1>
-
         <div className="ss:flex hidden md:mr-4 m4-0">
           <GetStarted />
         </div>
       </div>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Our team of experts uses a methodology to identify the credit cards most likely to fit your
+        needs. We examine annual percentage rates and fees.
+      </p>
+    </div>
+
+    {/* Right side */}
+    <div>
+      <img src={robot} alt="robot" className="w-[100%] h-[100%] relative z-[5]" />
+      <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
+      <div className="absolute z-[1] w-[80%] h-[80%] bottom-40 white__gradient round-full" />
+      <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
     </div>
   </section>
 )
